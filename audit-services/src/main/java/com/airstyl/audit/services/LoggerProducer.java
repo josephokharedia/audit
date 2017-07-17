cf8d1213406e70ee9e5554313bb689ce9f24706b
JOSEPH OKHAREDIA <JOSEPHOKHAREDIA@PERAGO.COM>
2017-07-17 16:47:18 +0200

    Improved

--- audit-services/src/main/java/com/airstyl/audit/services/LoggerProducer.java
+++ audit-services/src/main/java/com/airstyl/audit/services/LoggerProducer.java
@@ -1,16 +1,18 @@
 package com.airstyl.audit.services;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
-import java.util.logging.Logger;
 
 @Dependent
 public class LoggerProducer {
 
     @Produces
     public Logger createLogger(InjectionPoint injectionPoint) {
-        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+        return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
     }
 }
